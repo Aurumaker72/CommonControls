e47f209afccf086e2939aafa9dfabb683c029edf
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-12-25 11:12:55 +0100

    Improve button class

--- controls/button.cpp
+++ controls/button.cpp
@@ -9,18 +9,14 @@ namespace Button {
 
 	void register_global()
 	{
-		WNDCLASS AnsiWndCls;
+		WNDCLASS wndclass = {0};
 
-		AnsiWndCls.style = CS_DBLCLKS | CS_PARENTDC;
-		AnsiWndCls.lpfnWndProc = (WNDPROC)wndproc;
-		AnsiWndCls.cbClsExtra = 0;
-		AnsiWndCls.cbWndExtra = 0;
-		AnsiWndCls.hInstance = GetModuleHandle(0);
-		AnsiWndCls.hIcon = NULL;
-		AnsiWndCls.hCursor = LoadCursor(NULL, (LPTSTR)IDC_IBEAM);
-		AnsiWndCls.hbrBackground = NULL;
-		AnsiWndCls.lpszMenuName = NULL;
-		AnsiWndCls.lpszClassName = class_name;
+		wndclass.style = CS_DBLCLKS | CS_PARENTDC | CS_HREDRAW | CS_VREDRAW;
+		wndclass.lpfnWndProc = (WNDPROC)wndproc;
+		wndclass.hInstance = GetModuleHandle(nullptr);
+		wndclass.hCursor = LoadCursor(NULL, (LPTSTR)IDC_IBEAM);
+		wndclass.lpszClassName = class_name;
+		RegisterClass(&wndclass);
 	}
 
 }
