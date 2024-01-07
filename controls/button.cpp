2ce153f48db06bf1e88837694606cfe19c9f90b5
Alex <48759429+Aurumaker72@users.noreply.github.com>
2024-01-07 19:17:36 +0100

    Don't invalidate button on creation

--- controls/button.cpp
+++ controls/button.cpp
@@ -1,22 +1,43 @@
 #include "button.h"
+
+#include <assert.h>
 #include <Windows.h>
 
-namespace Button {
+namespace Button
+{
+    LRESULT CALLBACK wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+    {
+        switch (msg)
+        {
+        case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                RECT rect;
+                HDC hdc = BeginPaint(hwnd, &ps);
+                GetClientRect(hwnd, &rect);
 
-	LRESULT CALLBACK wndproc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
+                HBRUSH brush = CreateSolidBrush(RGB(255,0,0));
+                SelectObject(hdc, brush);
+                Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+                SelectObject(hdc, nullptr);
+                DeleteObject(brush);
+                
+                EndPaint(hwnd, &ps);
+                return 0;
+            }
+        }
+        return DefWindowProc(hwnd, msg, wparam, lparam);
+    }
 
-	void register_global()
-	{
-		WNDCLASS wndclass = {0};
-
-		wndclass.style = CS_DBLCLKS | CS_PARENTDC | CS_HREDRAW | CS_VREDRAW;
-		wndclass.lpfnWndProc = (WNDPROC)wndproc;
-		wndclass.hInstance = GetModuleHandle(nullptr);
-		wndclass.hCursor = LoadCursor(NULL, (LPTSTR)IDC_IBEAM);
-		wndclass.lpszClassName = class_name;
-		RegisterClass(&wndclass);
-	}
-
+    void register_global()
+    {
+        WNDCLASS wndclass = {0};
+        
+        wndclass.style = CS_GLOBALCLASS | CS_HREDRAW | CS_VREDRAW;
+        wndclass.lpfnWndProc = (WNDPROC)wndproc;
+        wndclass.hInstance = GetModuleHandle(nullptr);
+        wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+        wndclass.lpszClassName = class_name;
+        RegisterClass(&wndclass);
+    }
 }
